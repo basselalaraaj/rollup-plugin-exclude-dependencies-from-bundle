5aab6da8c1df8a9c4f96391176b82db14d23ec2b
Bassel Al Araaj <info@basselalaraaj.com>
2019-05-23 23:15:27 +0200

    Fix: making getexternal function more efficient

--- src/get-external.ts
+++ src/get-external.ts
@@ -1,21 +1,23 @@
 const path = require("path");
 
-export default function getExternal(modules: string[] = []): string[] | ((module: string) => boolean) {
-  try {
-    const packageFilePath: string = path.resolve(process.cwd(), "package.json");
-    const packageFile = require(packageFilePath);
-
-    const peerDependencies: { string: string } = packageFile.peerDependencies || {};
-    const dependencies: { string: string } = packageFile.dependencies || {};
-    const externalModules: string[] = [...modules, ...Object.keys(peerDependencies), ...Object.keys(dependencies)];
-
-    const regexps = externalModules.map(externalModule => new RegExp("^" + externalModule + "(\\/.+)*$"));
-    return module => {
-      return regexps.some(regexp => {
-        return regexp.test(module);
-      });
-    };
-  } catch (err) {
+const getDeps = (deps: { string: string }): string[] => {
+  if (!deps) {
     return [];
   }
-}
+
+  return Object.keys(deps);
+};
+
+const getExternal = (modules: string[] = []): string[] | ((module: string) => boolean) => {
+  const packageFilePath: string = path.resolve(process.cwd(), "package.json");
+  const packageFile = require(packageFilePath);
+
+  const peerDependencies = getDeps(packageFile.peerDependencies);
+  const dependencies = getDeps(packageFile.dependencies);
+  const externalModules: string[] = [...modules, ...peerDependencies, ...dependencies];
+
+  const regexps = externalModules.map(externalModule => new RegExp("^" + externalModule + "(\\/.+)*$"));
+  return module => regexps.some(regexp => regexp.test(module));
+};
+
+export default getExternal;
